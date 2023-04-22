5557a1ffb8254c8df239ec4e4c791b03e8978757
kinger83 <kinger83@outlook.com>
2023-04-22 18:18:17 +1000

    Ready to submit

--- MinDequesNumber.cs
+++ MinDequesNumber.cs
@@ -24,7 +24,7 @@ namespace MinDequesNumber
             for (int i = 1; i < data.Length; i++)
             {
                 // Setup variables to send and recieve from Determin Action
-                int[] actionArray = DeterminAction1(data, i);
+                int[] actionArray = DeterminAction(data, i);
                 int action = actionArray[0];
                 switch (action)
                 {
@@ -50,119 +50,10 @@ namespace MinDequesNumber
                 }
 
             }
-            PrintDeques();
+            //PrintDeques();
             return deques.Count;
 
         }// end solve
-
-
-        private static int[] DeterminAction2(int[] data, int index)
-        {
-            // calculate how many deques needed
-            for (int i = 0; i < data.Length; i++)
-            {
-
-            }
-
-
-
-
-            // temp retunr to block errors
-            return new int[] { 0, 0 };
-        }// end determinAction
-
-        private static int[] DeterminAction1(int[] data, int index)
-        {
-            // declare result array
-            int[] result = new int[2];
-
-
-            // do algorithms
-            for (int i = 0; i < deques.Count; i++)
-            {
-                // find which deque to use
-                int currentHigh = 0;
-                int currentLow = 0;
-                int toJudge = data[index];
-
-                int dequeToUseIndex = -1;
-                int minDiff = 50000000;
-                // find deque value is closest to
-                for (int j = 0; j < deques.Count; j++)
-                {
-                    if (toJudge > deques[j].Last.Value)
-                    {
-                        if (Math.Abs(toJudge - deques[j].Last.Value) < Math.Abs(minDiff))
-                        {
-                            minDiff = toJudge - deques[j].Last.Value;
-                            dequeToUseIndex = j;
-                            currentHigh = deques[j].Last.Value;
-                            currentLow = deques[j].First.Value;
-                        }
-
-                    }
-
-                    if (toJudge < deques[j].First.Value)
-                    {
-                        if (Math.Abs(deques[j].First.Value - toJudge) < Math.Abs(minDiff))
-                        {
-                            minDiff = toJudge - deques[j].First.Value;
-                            dequeToUseIndex = j;
-                            currentHigh = deques[j].Last.Value;
-                            currentLow = deques[j].First.Value;
-                        }
-
-                    }
-                } // end forloop finding correct deque
-
-
-
-
-                // process current value on selected deque
-                if (toJudge > currentHigh)
-                {
-                    bool inbetweenFound = false;
-                    // check for a value inbetween, if found set found bool to true
-                    for (int j = index + 1; j < data.Count(); j++)
-                    {
-                        if (data[j] > currentHigh && data[j] < toJudge)
-                        {
-                            inbetweenFound = true;
-                        }
-                    }
-                    // If not inbetween found return result
-                    if (!inbetweenFound)
-                    {
-                        result = new int[] { 1, dequeToUseIndex };
-                        return result;
-                    }
-                }
-
-                // check if value to process is lower than value at bottom of deque
-                else if (toJudge < currentLow)
-                {
-                    bool inbetweenFound = false;
-                    // check for a value inbetween, if found set found bool to true
-                    for (int j = index + 1; j < data.Count(); j++)
-                    {
-                        if (data[j] < currentLow && data[j] > toJudge)
-                        {
-                            inbetweenFound = true;
-                        }
-                    }
-                    // If not inbetween found return result
-                    if (!inbetweenFound)
-                    {
-                        result = new int[] { 2, dequeToUseIndex };
-                        return result;
-                    }
-                }
-
-
-            }
-            result = new int[] { 3, -1 };
-            return result;
-        }// end determinAction
 
         private static int[] DeterminAction(int[] data, int index)
         {
