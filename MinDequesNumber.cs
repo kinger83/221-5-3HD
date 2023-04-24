3a514632451f580d1e478e4f18d0cff5863a5e79
kinger83 <kinger83@outlook.com>
2023-04-24 10:32:32 +1000

    Final working version

--- MinDequesNumber.cs
+++ MinDequesNumber.cs
@@ -10,15 +10,21 @@ namespace MinDequesNumber
     {
         // Create a list of linked lists names deques
         public static List<LinkedList<int>> deques = new List<LinkedList<int>>();
+        public static List<int> dequeOrderList = new List<int>();
+
 
         public static int Solve(int[] data)
         {
+
             // Clear deques list
             deques.Clear();
 
             // Add first deque
             deques.Add(new LinkedList<int>());
             deques[0].AddFirst(data[0]);
+            dequeOrderList.Add(0);
+
+
 
             // Cycle through input data from index 1 and assign positions in deques
             for (int i = 1; i < data.Length; i++)
@@ -28,12 +34,12 @@ namespace MinDequesNumber
                 int action = actionArray[0];
                 switch (action)
                 {
-                    // Case 1, Insert first in deque [,x]
+                    // Case 1, Insert to High in deque [,x]
                     case 1:
                         deques[actionArray[1]].AddLast(data[i]);
                         break;
 
-                    // Case 2, Insert Last in deque [,x]
+                    // Case 2, Insert Low in deque [,x]
                     case 2:
                         deques[actionArray[1]].AddFirst(data[i]);
                         break;
@@ -55,71 +61,100 @@ namespace MinDequesNumber
 
         }// end solve
 
+
         private static int[] DeterminAction(int[] data, int index)
         {
-            // declare result array
-            int[] result = new int[2];
-
-
-            // do algorithms
+            int toProcess = data[index];
             for (int i = 0; i < deques.Count; i++)
             {
-                // check if to judge is higher or lower than current deque
-                int currentHigh = deques[i].Last.Value;
-                int currentLow = deques[i].First.Value;
-                int toJudge = data[index];
-                // check if value to process is higher than value at top of deque
-                if (toJudge > currentHigh)
+                int dequeHigh = deques[i].Last.Value;
+                // If larget than Last (Highest in deque)
+                if (toProcess > deques[i].Last.Value)
                 {
                     bool inbetweenFound = false;
-                    // check for a value inbetween, if found set found bool to true
-                    for (int j = index + 1; j < data.Count(); j++)
+                    // check no value inbetween the 2
+                    for (int j = 0; j < data.Length; j++)
                     {
-                        if (data[j] > currentHigh && data[j] < toJudge)
+                        if (data[j] < toProcess && data[j] > deques[i].Last.Value)
                         {
                             inbetweenFound = true;
                         }
+
                     }
-                    // If not inbetween found return result
                     if (!inbetweenFound)
                     {
-                        result = new int[] { 1, i };
-                        return result;
+                        return new int[] { 1, i };
                     }
                 }
 
-                // check if value to process is lower than value at bottom of deque
-                else if (toJudge < currentLow)
+                // Ifsmaller  than First (Lowest in deque)
+                if (toProcess < deques[i].First.Value)
                 {
                     bool inbetweenFound = false;
-                    // check for a value inbetween, if found set found bool to true
-                    for (int j = index + 1; j < data.Count(); j++)
+                    // check no value inbetween the 2
+                    for (int j = 0; j < data.Length; j++)
                     {
-                        if (data[j] < currentLow && data[j] > toJudge)
+                        if (data[j] > toProcess && data[j] < deques[i].First.Value)
                         {
                             inbetweenFound = true;
                         }
+
                     }
-                    // If not inbetween found return result
                     if (!inbetweenFound)
                     {
-                        result = new int[] { 2, i };
-                        return result;
+                        return new int[] { 2, i };
                     }
                 }
+
             }
-            result = new int[] { 3, -1 };
-            return result;
-        }// end determinAction
+            return new int[] { 3, 0 };
+        } // end DeterminAction
+
+        private static void CalculateDequeOrder()
+        {
+            List<int[]> dequeValues = new List<int[]>();
+            dequeOrderList = new List<int>();
+
+            for (int j = 0; j < deques.Count; j++)
+            {
+                int[] temp = new int[3];
+                temp[0] = deques[j].First.Value;
+                temp[1] = j;
+                temp[2] = 0;
+                dequeValues.Add(temp);
+            }
+
+            int i = 0;
+            while (i < deques.Count)
+            {
+                int minValue = 9999999;
+                int minDeque = -1;
+                for (int d = 0; d < dequeValues.Count; d++)
+                {
+                    if (dequeValues[d][2] == 0 && dequeValues[d][0] < minValue)
+                    {
+                        minValue = dequeValues[d][0];
+                        minDeque = d;
+                    }
+                }
+                dequeOrderList.Add(minDeque);
+                dequeValues[minDeque][2] = 1;
+                i++;
+            }
+        }// end Calculate Order
+
         private static void PrintDeques()
         {
+            CalculateDequeOrder();
             string toPrint = "";
-            foreach (var deque in deques)
+
+            for (int i = 0; i < dequeOrderList.Count; i++)
             {
-                foreach (var value in deque)
+                foreach (var value in deques[dequeOrderList[i]])
                 {
                     toPrint += (value.ToString() + ", ");
                 }
+
             }
             Console.WriteLine(toPrint);
         }
